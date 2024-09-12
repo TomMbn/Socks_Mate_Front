2cd8c82293feb77ad692cb89fa51d3d3e8bef3bf
Tom Mauboussin <tom.mauboussin@icloud.com>
2024-09-12 11:04:05 +0200

    Styles/Icones : Modifs de style et ajout des icones

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import '../styles/Home.css';  // pour ajouter des styles spécifiques si besoin
 
 const Home: React.FC = () => {
   return (
-    <div className="Container">
+    <div className="homeContainer">
       <div className="homeContent">
         <h1 className="Title">Sock's Mate</h1>
         <div className="HomeLink">
--- src/pages/Like.tsx
+++ src/pages/Like.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Like.css';
+import Navbar from '../components/Navbar';
 
 const Like: React.FC = () => {
     const [activeTab, setActiveTab] = useState<'ilsMaiment' | 'jaime'>('ilsMaiment');
@@ -11,6 +12,12 @@ const Like: React.FC = () => {
       { id: 2, sockName: 'Chaussette Bleue', sockSize: 'Taille 42', description: 'Chaussette sportive pour un meilleur maintien.', imageUrl: '/images/blue-sock.jpg' },
       { id: 3, sockName: 'Chaussette Verte', sockSize: 'Taille 38', description: 'Chaussette douce et écologique.', imageUrl: '/images/green-sock.jpg' },
     ];
+
+    const socksThatILiked = [
+      { id: 1, sockName: 'Chaussette R', sockSize: 'Taille 40', description: 'Chaussette confortable en coton.', imageUrl: '/images/red-sock.jpg' },
+      { id: 2, sockName: 'Chaussette B', sockSize: 'Taille 42', description: 'Chaussette sportive pour un meilleur maintien.', imageUrl: '/images/blue-sock.jpg' },
+      { id: 3, sockName: 'Chaussette V', sockSize: 'Taille 38', description: 'Chaussette douce et écologique.', imageUrl: '/images/green-sock.jpg' },
+    ];
   
     const handleTabChange = (tab: 'ilsMaiment' | 'jaime') => {
       setActiveTab(tab);
@@ -20,7 +27,7 @@ const Like: React.FC = () => {
       navigate(`/sock/${id}`);
     };
   
-    return (
+    return (<>
       <div className="like">
         <h1>Sock's Mate</h1>
         
@@ -43,8 +50,20 @@ const Like: React.FC = () => {
               <span className="view-profile">Découvrez son profil</span>
             </div>
           ))}
+          {activeTab === 'jaime' && socksThatILiked.map((sock) => (
+            <div key={sock.id} className="sock-banner" onClick={() => handleProfileClick(sock.id)}>
+              <div className="banner-info">
+                <span>{sock.sockName}</span>
+                <span>{sock.sockSize}</span>
+              </div>
+              <span className="view-profile">Découvrez son profil</span>
+            </div>
+          ))}
         </div>
+        
       </div>
+      <Navbar />
+      </>
     );
   };
   
